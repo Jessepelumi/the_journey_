2bba46c024d76e8b5f0543d824ebf665f586c6d5
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2025-10-30 17:16:44 +0000

    added a method that uses swaps instead of python insert method

--- arrays/insertion_sort.py
+++ arrays/insertion_sort.py
@@ -4,14 +4,29 @@ def insertionSort(arr:list):
     n = len(arr)
 
     for i in range(1, n):
-        
         for j in range(i):
             if arr[j] > arr[i]:
-                val = arr.pop(i)
-                arr.insert(j, val)
+                sort = arr.pop(i)
+                arr.insert(j, sort)
 
     return arr
 
+def insertionSortSwap(arr:list):
+    n = len(arr)
+
+    for i in range(1, n):
+        for j in range(i):
+            if arr[j] > arr[i]:
+                arr[j], arr[i] = arr[i], arr[j]
+
+    return arr
+
+def insertionSortOptemized(arr:list):
+    pass
+
 # Usage
 arr = [10, 3, 4, 6, 2, 7, 5, 1]
-print(insertionSort(arr))
+print(f"Sorted: {insertionSort(arr.copy())}")
+print(f"Swap sorted: {insertionSortSwap(arr.copy())}")
+
+# arr[j], arr[i] = arr[i], arr[j]
