5a32e5744151dcd90858061aacdd65089c72bedc
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2025-11-03 14:52:03 +0000

    optimized the insertion sort algorithm

--- arrays/insertion_sort.py
+++ arrays/insertion_sort.py
@@ -22,9 +22,24 @@ def insertionSortSwap(arr:list):
     return arr
 
 def insertionSortOptemized(arr:list):
-    pass
+    n = len(arr)
+
+    for i in range(1, n):
+        insert_index = i
+        current_value = arr[i]
+        for j in range(i-1, -1, -1):
+            if arr[j] > current_value:
+                arr[j+1] = arr[j]
+                insert_index = j
+            else:
+                break
+        
+        arr[insert_index] = current_value
+
+    return arr
 
 # Usage
 arr = [10, 3, 4, 6, 2, 7, 5, 1]
 print(f"Sorted: {insertionSort(arr.copy())}")
 print(f"Swap sorted: {insertionSortSwap(arr.copy())}")
+print(f"Optimized: {insertionSortOptemized(arr.copy())}")
